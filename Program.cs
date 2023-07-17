9af296c05d104cf31faef1ba1a4131ae45c7aa44
aryark <aryark3112@gmail.com>
2023-07-17 12:31:14 +0530

    first commit

--- Program.cs
+++ Program.cs
@@ -28,6 +28,36 @@
 //    }
 //}
 
+//using System;
+
+////Call by Parameter
+//namespace _17JulyParameters
+//{
+//    public class Calc
+//    {
+//        public void Increment( ref int num)
+//        {
+//            Console.WriteLine("Value of num inside Method before increment: \t" + num);
+//            num += 5;
+//            Console.WriteLine("Value of num inside Method After increment: \t" + num);
+//        }
+//    }
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            int myNum;
+//            Console.WriteLine("Enter a number");
+//            myNum = int.Parse(Console.ReadLine());
+//            Console.WriteLine("myNum in Main before Method Call is: \t" + myNum);
+//            Calc calc = new Calc();
+//            calc.Increment( ref myNum);
+//            Console.WriteLine("myNum in Main After Method Call is: \t" + myNum);
+//            Console.ReadKey();
+//        }
+//    }
+//}
+
 using System;
 
 //Call by Parameter
@@ -35,24 +65,20 @@ namespace _17JulyParameters
 {
     public class Calc
     {
-        public void Increment( ref int num)
+        public void BonusCalc(double salary, ref double bonus)
         {
-            Console.WriteLine("Value of num inside Method before increment: \t" + num);
-            num += 5;
-            Console.WriteLine("Value of num inside Method After increment: \t" + num);
+            bonus = salary * 0.2;
         }
     }
     internal class Program
     {
         static void Main(string[] args)
         {
-            int myNum;
-            Console.WriteLine("Enter a number");
-            myNum = int.Parse(Console.ReadLine());
-            Console.WriteLine("myNum in Main before Method Call is: \t" + myNum);
+            double salary = 50000;
+            double bonus = 500;
             Calc calc = new Calc();
-            calc.Increment( ref myNum);
-            Console.WriteLine("myNum in Main After Method Call is: \t" + myNum);
+            calc.BonusCalc(salary, ref bonus);
+            Console.WriteLine($"Salary: \t {salary} and Bonus : {bonus}");
             Console.ReadKey();
         }
     }
